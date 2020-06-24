754a359a5be4642ab77da3b4ef7f05c567dd001d
nshakoori <naeimeh@gmail.com>
2020-06-23 23:19:38 -0400

    removed some comments and added procfile

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,5 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :punch_records, only: [:index, :create, :update]
-    resources :punch_cards, only: [:new, :create] #do
-    #   member do 
-    #     get 'punch_in', to: "punch_cards#punch_in"
-    #     get 'punch_out', to: "punch_cards#punch_out"
-    #   end
-    # end
+    resources :punch_cards, only: [:new, :create]
 end
